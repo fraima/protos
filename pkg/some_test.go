d7a475dea9a5701c7bc186dffb7dae80000b8362
EvgenyGRI <eugene_gri@mail.ru>
2022-08-25 15:28:23 +0300

    add closable client constructor; add swagger utils

---
+++ pkg/some_test.go
@@ -0,0 +1,24 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/H-BF/protos/pkg/api/sgroups"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestSwaggerUtil(t *testing.T) {
+	var u SwaggerUtil[sgroups.SecGroupServiceServer]
+	s, e := u.GetSpec()
+	require.NoError(t, e)
+	require.NotNil(t, s)
+}
+
+func TestClosableClient(t *testing.T) {
+	var conn *grpc.ClientConn
+	var c ClosableClient[sgroups.SecGroupServiceClient]
+	err := c.Init(conn)
+	require.NoError(t, err)
+	require.NotNil(t, c.C)
+}
